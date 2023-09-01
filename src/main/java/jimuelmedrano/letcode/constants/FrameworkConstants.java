c6c027b031f9c9dea7e140285f33a61885a3be8e
Jimuel Renzo Medrano <jimuelrenzomedrano@gmail.com>
2023-09-01 19:39:20 +0800

    set headless to true for CI

--- src/main/java/jimuelmedrano/letcode/constants/FrameworkConstants.java
+++ src/main/java/jimuelmedrano/letcode/constants/FrameworkConstants.java
@@ -3,6 +3,6 @@ package jimuelmedrano.letcode.constants;
 public class FrameworkConstants {
     public static String INPUT_FULL_NAME = "Jimuel Renzo Medrano";
     public static int TIMEOUT = 60;
-    public static boolean isHeadless = false;
+    public static boolean isHeadless = true;
     public static String BASE_URL = "https://letcode.in/test";
 }
