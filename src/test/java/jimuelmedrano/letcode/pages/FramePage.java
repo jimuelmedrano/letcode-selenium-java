56e78e6eab00aa8c2087153a9a46d46bb3b1f8b0
Jimuel Renzo Medrano <jimuelrenzomedrano@gmail.com>
2023-09-03 17:02:55 +0800

    letcode frame scenarios and add run time

---
+++ src/test/java/jimuelmedrano/letcode/pages/FramePage.java
@@ -0,0 +1,15 @@
+package jimuelmedrano.letcode.pages;
+
+import org.openqa.selenium.By;
+
+public class FramePage {
+
+    public static By framePageLink = By.xpath("//a[normalize-space()='Inner HTML']");
+    public static By framePageView = By.xpath("//p[normalize-space()='Frame']");
+
+    public static By firstFrame = By.id("firstFr");
+    public static By firstName = By.name("fname");
+    public static By lastName = By.name("lname");
+    public static By innerFrame = By.xpath("//iframe[@src='innerFrame']");
+    public static By email = By.name("email");
+}
--- src/test/java/jimuelmedrano/letcode/utils/Annotations.java
+++ src/test/java/jimuelmedrano/letcode/utils/Annotations.java
@@ -3,12 +3,18 @@ package jimuelmedrano.letcode.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.*;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Annotations {
+    Instant startTime;
+    Instant endTime;
     public DriverSetup ds = new DriverSetup();
     @BeforeSuite
     public void beforeAll() {
         System.out.println("Starting Test...");
-        //TODO: start time
+        //Execution Start Time
+        startTime = Instant.now();
     }
     @BeforeClass
     public void setUp() {
@@ -21,7 +27,9 @@ public class Annotations {
     @AfterSuite
     public void afterALl() {
         ds.driver.quit();
-        System.out.println("Test Done");
-        //TODO: print difference of start and end time to get run time
+        System.out.println("Test Done!");
+        endTime = Instant.now();
+        long time = Duration.between(startTime, endTime).toSeconds();
+        System.out.println("RUN TIME: " + time +"s");
     }
 }
