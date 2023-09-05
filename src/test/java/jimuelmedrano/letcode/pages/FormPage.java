0463722fa79e0f9fdbfd0d8512059a8a67a5e1ea
Jimuel Renzo Medrano <jimuelrenzomedrano@gmail.com>
2023-09-05 20:55:23 +0800

    letcode form scenario

--- src/test/java/jimuelmedrano/letcode/pages/FormPage.java
+++ src/test/java/jimuelmedrano/letcode/pages/FormPage.java
@@ -6,6 +6,18 @@ public class FormPage {
 
     public static By formPageLink = By.xpath("//a[normalize-space()='All in One']");
     public static By formPageView = By.xpath("//p[normalize-space()='Forms']");
-    public static By homePageButton = By.id("home");
-    public static By multipleTabButton = By.id("multi");
+    public static By firstNameInput = By.id("firstname");
+    public static By lastNameInput = By.id("lasttname");
+    public static By emailInput = By.id("email");
+    public static By countryCodeSelect = By.xpath("//label[@id='countrycode']//parent::div//select");
+    public static By phoneInput = By.id("Phno");
+    public static By addressLine1Input = By.id("Addl1");
+    public static By addressLine2Input = By.id("Addl2");
+    public static By stateInput = By.id("state");
+    public static By postalInput = By.id("postalcode");
+    public static By countrySelect = By.xpath("//label[@id='country']//parent::div//select");
+    public static By dateOfBirthInput = By.id("Date");
+    public static By genderRadioElements = By.name("gender");
+    public static By agreeToTerms = By.xpath("//input[@type='checkbox']");
+    public static By submitButton = By.xpath("//input[@type='submit']");
 }
--- src/test/java/jimuelmedrano/letcode/tests/FormTest.java
+++ src/test/java/jimuelmedrano/letcode/tests/FormTest.java
@@ -6,34 +6,74 @@ import jimuelmedrano.letcode.pages.FormPage;
 import jimuelmedrano.letcode.pages.WindowPage;
 import jimuelmedrano.letcode.utils.Annotations;
 import jimuelmedrano.letcode.utils.HttpUtils;
+import jimuelmedrano.letcode.utils.JavaUtils;
 import org.json.JSONObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.page.Page;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.awt.*;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 public class FormTest extends Annotations {
 
     @BeforeMethod
     public void open(){
-//        ds.driver.get(FrameworkConstants.BASE_URL);
-//        PageActions.scrollToElement(ds.driver, FormPage.formPageView);
-//        PageActions.click(ds.driver, FormPage.formPageLink);
+        ds.driver.get(FrameworkConstants.BASE_URL);
+        PageActions.scrollToElement(ds.driver, FormPage.formPageView);
+        PageActions.click(ds.driver, FormPage.formPageLink);
     }
 
     @Test(priority = 1)
-    public void inputForm() throws InterruptedException, IOException {
+    public void inputForm() throws InterruptedException, IOException, ParseException {
         JSONObject randomUser = HttpUtils.GET(FrameworkConstants.RANDOM_USER_BASE_URL,FrameworkConstants.RANDOM_USER_ENDPOINT);
-        System.out.println(randomUser.toString());
-        System.out.println(randomUser.getString("first_name"));
-
+        //Input First Name from API
+        PageActions.inputText(ds.driver,FormPage.firstNameInput,randomUser.getString("first_name"));
+        //Input Last Name from API
+        PageActions.inputText(ds.driver,FormPage.lastNameInput,randomUser.getString("last_name"));
+        //Input Email from API
+        PageActions.getElement(ds.driver,FormPage.emailInput).clear();
+        PageActions.inputText(ds.driver,FormPage.emailInput,randomUser.getString("email"));
+        //Select Country Code using random number generator
+        List<WebElement> countryCodes = PageActions.getAllSelectOptions(ds.driver,FormPage.countryCodeSelect);
+        int randomCountryIndex = JavaUtils.randomNumber(3,countryCodes.size());
+        PageActions.selectByIndex(ds.driver,FormPage.countryCodeSelect,randomCountryIndex);
+        //Input Phone Number using random number generator
+        long randomPhoneNumber = JavaUtils.randomLongNumber(9000000000L,9999999999L);
+        PageActions.inputText(ds.driver,FormPage.phoneInput, ""+randomPhoneNumber);
+        //Input Address Line 1 from API
+        JSONObject address = randomUser.getJSONObject("address");
+        PageActions.inputText(ds.driver,FormPage.addressLine1Input,address.getString("street_address"));
+        //Input Address Line 2 from API
+        PageActions.inputText(ds.driver,FormPage.addressLine2Input,address.getString("city"));
+        //Input State from API
+        PageActions.inputText(ds.driver,FormPage.stateInput,address.getString("state"));
+        //Input Postal Code from API
+        PageActions.inputText(ds.driver,FormPage.postalInput,address.getString("zip_code"));
+        //Select Country from API
+        PageActions.selectByValue(ds.driver,FormPage.countrySelect,address.getString("country"));
+        //Select Date of Birth from API
+        String dateString = JavaUtils.formattedDate(randomUser.getString("date_of_birth"));
+        PageActions.scrollToElement(ds.driver,FormPage.dateOfBirthInput);
+        PageActions.inputText(ds.driver,FormPage.dateOfBirthInput,dateString);
+        //Select Random Gender using random number generator
+        List<WebElement> genderElements = PageActions.getElements(ds.driver,FormPage.genderRadioElements);
+        int randomGender = JavaUtils.randomNumber(0,2);
+        genderElements.get(randomGender).click();
+        //Click Agree to Terms and Conditions
+        PageActions.click(ds.driver,FormPage.agreeToTerms);
+        //Click Submit Button
+        PageActions.click(ds.driver,FormPage.submitButton);
     }
 
     @Test(priority = 2)
     public void testPost() throws InterruptedException, IOException {
-
         //Sample Data for post API
         //The sample post API will return the same parameter you sent as response
         var body = new HashMap<String, String>() {{
